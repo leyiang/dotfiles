65346a3f70aca2195be167b0751b584a7102b4cd
Yiang <yiangforwork@gmail.com>
2024-11-24 22:00:52 +0800

    update: set color scheme

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,22 +1,27 @@
 local scripts = {
-    "options", -- vim设置, vimrc的东西放这
-    "keymaps", -- vim按键映射
-    "plugins", -- 安装的插件
+    "options",                -- vim设置, vimrc的东西放这
+    "keymaps",                -- vim按键映射
+    "plugins",                -- 安装的插件
 
-    "clip-image-config",
-    "nvim-tree", -- 文件选择
-    "telescope", -- 快速跳转文件
+    "nvim-tree",              -- 文件选择
+    "telescope",              -- 快速跳转文件
 
-    "comment", -- 快速注释
+    "comment",                -- 快速注释
+    "toggle-val",             -- 快速切换 true-false, 等状态
+    "treesitter",             -- AST插件, 其它各种插件都需要treesitter的支持
+    "cmp",                    -- 代码提示插件
+    "neogen",                 -- 注释生成 jsdoc
+
+    "conform",                -- formatter
+    "mason",                  -- mason管理器
+
     "chinese-char-navigator", -- 中文首字母跳转
-    "toggle-val", -- 快速切换 true-false, 等状态
-
-    -- "color",
-    -- "cmp",
+    "color",                  -- nvim配色
     -- "lsp",
-    -- "treesitter",
     -- "autopairs",
     -- "snippets",
+    -- "clip-image-config",
+
 }
 
 for k, script in pairs(scripts) do
--- config/nvim/lua/user/color.lua
+++ config/nvim/lua/user/color.lua
@@ -1,10 +1,2 @@
-vim.cmd "colorscheme default"
-
-local color = "tokyonight"
-
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. color)
-
-if not status_ok then
-    vim.notify("colorscheme " .. color .. " not found")
-    return
-end
+-- vim.cmd "colorscheme default"
+-- vim.cmd "colorscheme catppuccin"
