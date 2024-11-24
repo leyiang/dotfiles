c4d38dbfc51b59cd01037b288b7a2c0747c245a4
Yiang <yiangforwork@gmail.com>
2024-11-25 07:41:46 +0800

    update: override toml icon for nvim-tree

--- config/nvim/lua/user/nvim-tree.lua
+++ config/nvim/lua/user/nvim-tree.lua
@@ -1,4 +1,13 @@
-require("nvim-web-devicons").setup()
+require("nvim-web-devicons").setup({
+    override = {
+        toml = {
+            icon = "",
+            color = "#428850",
+            cterm_color = "65",
+            name = "Zsh",
+        },
+    },
+})
 
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
@@ -12,21 +21,21 @@ vim.opt.termguicolors = true
 
 -- OR setup with some options
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = false,
-  },
-  update_focused_file = {
-    enable = true,
+    sort_by = "case_sensitive",
+    view = {
+        width = 30,
+    },
+    renderer = {
+        group_empty = true,
+    },
+    filters = {
+        dotfiles = false,
+    },
+    update_focused_file = {
+        enable = true,
+        update_cwd = true,
+    },
     update_cwd = true,
-  },
-  update_cwd = true
 })
 
 vim.g.nvim_tree_respect_buf_cwd = 1
