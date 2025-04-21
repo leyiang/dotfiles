252b42a087a8c01337b044d0abdf8378b81ea143
Yiang <yiangforwork@gmail.com>
2025-04-21 13:13:04 +0800

    update: lots of yazi conf update

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -1,5 +1,5 @@
-require("relative-motions"):setup({
-    show_numbers="relative",
-    show_motion = true,
-})
+-- require("relative-motions"):setup({
+--     show_numbers="relative",
+--     show_motion = true,
+-- })
 
